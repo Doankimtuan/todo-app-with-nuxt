a4e62939ceaa9a835b019fb30ef2f6f69d2cbfb1
Doan Tuan <doantuan@Doans-MacBook-Air.local>
2022-09-15 16:47:12 +0700

    Create todo page

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+      browser: true
+    },
+    extends: [
+      "plugin:vue/recommended",
+      "eslint:recommended",
+      "prettier/vue",
+      "plugin:prettier/recommended"
+    ],
+    rules: {
+      "vue/component-name-in-template-casing": ["error", "PascalCase"],
+      "no-console": "off",
+      "no-debugger": "off"
+    },
+    globals: {
+      $nuxt: true
+    },
+    parserOptions: {
+      parser: "babel-eslint"
+    }
+  };
